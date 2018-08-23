937917c17b519c169b75ea5b4b2ca46c4ef512bd
David <davidneios25@gmail.com>
2018-08-23 13:10:32 -0400

    HeroSlider

--- frontend/src/components/heroSlider.js
+++ frontend/src/components/heroSlider.js
@@ -7,31 +7,31 @@ import './slider-styles.css';
 
 const content = [
 	{
-		title: 'Vulputate Mollis Ultricies Fermentum Parturient',
+		title: 'Amis du Jardin',
 		description:
-		'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Luan Gjokaj',
-		userProfile: 'https://i.imgur.com/JSW6mEk.png'
+		'Depuis 1975, les Amis du Jardin botanique de Montréal soutiennent la mission culturelle, éducative et scientifique du Jardin botanique de Montréal.Par l’entremise de diverses activités, les Amis du Jardin s’emploient à diffuser au grand public les connaissances relatives à la botanique et à l’horticulture.',
+		button: 'read more',
+		image: './images/banner/jardin.jpg',
+		user: 'AmisDuJardin',
+		userProfile: './images/amisdujardinLogo.PNG'
 	},
 	{
-		title: 'Tortor Dapibus Commodo Aenean Quam',
+		title: 'Canada World Youth',
 		description:
-		'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
+		'Canada World Youth offers world-renowned international volunteer programs to youth from Canada and abroad who, through their participation in community-driven development projects, acquire the leadership skills that allow them to become agents of change.',
 		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'Erich Behrens',
-		userProfile: 'https://i.imgur.com/0Clfnu7.png'
+		image: './images/banner/cwy.jpg',
+		user: 'CanadaWorldYouth',
+		userProfile: './images/canadaworldyouthLogo.PNG'
 	},
 	{
-		title: 'Phasellus volutpat metus',
+		title: 'Food Secure Canada',
 		description:
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Bruno Vizovskyy',
-		userProfile: 'https://i.imgur.com/4KeKvtH.png'
+		'Food Secure Canada is a pan-Canadian alliance of organizations and individuals working together to advance food security and food sovereignty through three inter-locking goals: zero hunger, healthy and safe food, and sustainable food systems.',
+		button: 'Read More',
+		image: './images/banner/foodsc.PNG',
+		user: 'FoodSecureCanada',
+		userProfile: './images/foodsecurecanadaLogo.PNG'
 	}
 ];
 class HeroSlider extends Component {
