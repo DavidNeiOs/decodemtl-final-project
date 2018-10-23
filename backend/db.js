94b7ebdc7aa5738392a97d940f7eb5e98ef38ecd
David <davidneios25@gmail.com>
2018-10-23 14:56:42 -0400

    proxy changes

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,30 @@
-var MongoClient = require('mongodb').MongoClient
+var MongoClient = require("mongodb").MongoClient;
 
 var state = {
-  db: null,
-}
+  db: null
+};
 
 exports.connect = function(url, done) {
-  if (state.db) {return done()}
+  if (state.db) {
+    return done();
+  }
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) {return done(err)}
-    state.db = db;
-    done();
-  })
-}
+  MongoClient.connect(
+    url,
+    { useNewUrlParser: true },
+    function(err, db) {
+      if (err) {
+        return done(err);
+      }
+      state.db = db;
+      done();
+    }
+  );
+};
 
 exports.get = function() {
-  return state.db;  
-}
+  return state.db;
+};
 
 exports.close = function(done) {
   if (state.db) {
@@ -24,6 +32,6 @@ exports.close = function(done) {
       state.db = null;
       state.mode = null;
       done(err);
-    })
+    });
   }
-}
+};
--- frontend/src/components/orgHomePage.js
+++ frontend/src/components/orgHomePage.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
-import ConnectedOrgNavBar from './orgNavBar.js'
-import Footer from './footer.js'
-import OrgHomeItemDisp from './orgHomeDisp.js'
-import BuyerHomeDisplay from './buyerHomDisplay.js'
-import { Icon, Segment, Header ,Divider} from 'semantic-ui-react'
-
+import React, { Component } from "react";
+import ConnectedOrgNavBar from "./orgNavBar.js";
+import Footer from "./footer.js";
+import OrgHomeItemDisp from "./orgHomeDisp.js";
+import BuyerHomeDisplay from "./buyerHomDisplay.js";
+import { Icon, Segment, Header, Divider } from "semantic-ui-react";
 
 class OrgHomePage extends Component {
   render() {
     return (
       <div className="App">
         <div>
-          <ConnectedOrgNavBar/>
-          </div>
-          
-        <Header as='h2' icon='smile' content='Welcome back !' textAlign='center'/>
-                <Divider/>
+          <ConnectedOrgNavBar />
+        </div>
+
+        <Header
+          as="h2"
+          icon="smile"
+          content="Welcome back !"
+          textAlign="center"
+        />
+        <Divider />
         <div>
           <OrgHomeItemDisp />
-
         </div>
         <div>
-         
           <Segment inverted>
-          <Header as='h2' icon={<Icon loading size='big' name='globe' color='white' />} content='Shop All Products' textAlign='center'/>
+            <Header
+              as="h2"
+              icon={<Icon loading size="big" name="globe" color="white" />}
+              content="Shop All Products"
+              textAlign="center"
+            />
           </Segment>
-          
 
-          
-          <BuyerHomeDisplay/>
+          <BuyerHomeDisplay />
         </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
 }
 function mapStateToProps(state) {
   return {
-      items: state.items,
-      currOrg : state.orgId,
-      org: state.currentOrg,
-  }
+    items: state.items,
+    currOrg: state.orgId,
+    org: state.currentOrg
+  };
 }
 export default OrgHomePage;
